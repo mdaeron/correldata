5467b63c73a9923cd0c4ac45fe37717e24235f4e
mdaeron <mathieu@daeron.fr>
2024-10-29 23:30:03 +0100

    Add doc and dev pixi features

--- build_doc.py
+++ build_doc.py
@@ -2,6 +2,6 @@ import pdoc
 
 pdoc.render.configure(template_directory = 'pdoc_templates', search = False)
 
-with open('../docs/index.html', 'w') as fid:
+with open('docs/index.html', 'w') as fid:
 	fid.write(pdoc.pdoc('correldata'))
 
